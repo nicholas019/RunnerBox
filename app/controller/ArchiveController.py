c2b8f4567650964143a151e8fab15cf7095fa049
maru <sorkihg@gamil.com>
2024-08-04 18:04:05 +0900

    스웨거 설명 추가

--- app/controller/ArchiveController.py
+++ app/controller/ArchiveController.py
@@ -5,7 +5,7 @@ from app.service.ArchiveService import ArchiveService
 
 router = APIRouter()
 
-@router.get("/archive")
+@router.get("/archive", summary="가장가까운 물품보관소 위치정보 조회", description="<p>위도와 경도 정보를 받아 가장 가까운 물품보관소 위치정보를 반환합니다.</p> <p>쿼리 스트링으로 latitude와 longitude를 받습니다.</p> <p>ex) /archive?latitude=37.1234&longitude=127.1234</p>")
 async def read_item(
         latitude: float = Query(..., description="Latitude of the location"),
         longitude: float = Query(..., description="Longitude of the location"),
@@ -18,13 +18,13 @@ async def read_item(
     return response
 
 
-@router.get("/archive/search")
+@router.get("/archive/search", summary="지하철역 이름으로 보관소의 위치정보를 조회", description="<p>지하철역이름으로 검색하면 해당역의 물품보관소가있는 역이름과 출구번호를 반환합니다</p> <p>쿼리 스트링으로 subway를 받습니다.</p> <p>ex) /archive/search?subway_name=뚝섬역</p>")
 async def read_item(
         subway_name: str = Query(..., description="subway_name"),
         db: Session = Depends(get_db)
 ):
     archive_service = ArchiveService(db)
     response = archive_service.search_subway_name(subway_name)
-    if response is None:
-        raise HTTPException(status_code=404, detail="response not found")
+    if len(response) == 0:
+        raise HTTPException(status_code=404, detail="archive not found")
     return response
